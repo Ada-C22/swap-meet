45730b59bcb34e83c860a0f507a5b36fe0be1793
Amber <110812972+Msambere@users.noreply.github.com>
2024-09-30 16:59:03 -0500

    Add blank functions to class files based on README
    Created functions in the vendor and item classes following the require-
    ments of the README: include function name, parameters, and "pass" in
    the body

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
 class Item:
-    pass
+    def __init__(self):
+        pass
+
+    def get_category(self):
+        pass
+    def __str__(self):
+        pass
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,26 @@
 class Vendor:
-    pass
+    def __init__(self, inventory):
+        pass
+        
+    def add(self):
+        pass
+    def remove(self):
+        pass
+
+    # Wave 2
+
+    def get_by_id(self):
+        pass
+
+    #Wave 3
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        pass
+
+    #Wave 4
+
+    def swap_first_item(self, other_vender):
+        pass
+
+    #Wave 5
+    
