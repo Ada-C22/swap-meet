a1c0439337945f23e915a72f8697f0be4c822419
Priyanka <pri.ppk@gmail.com>
2024-09-30 12:11:21 -0700

    Add Vendor class and unit test for wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, new_item_added):
+        self.inventory.append(new_item_added)
+        return new_item_added
+    
+    def remove(self, item_to_remove):
+        if item_to_remove not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item_to_remove)
+            return item_to_remove
