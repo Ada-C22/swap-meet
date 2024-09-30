beffcb4d4e44158213029d197f8f2fd2a9f0e1a0
Tami <tamigaertner@outlook.com>
2024-09-30 15:08:08 -0400

    work wave 1 vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,21 @@
+#
+
 class Vendor:
-    pass
+    def __init__(self, inventory):
+        self.inventory = inventory
+    
+    def __contains__(self, item):
+        return self.inventory[0] <= item <= self.inventory[len(self.inventory-1)]
+        
+    
+    def add(self, inventory, item):
+        inventory.append(item)
+        return item
+        
+    
+    def remove(self, item, inventory):
+        
+        for item in inventory:
+            if self.item in inventory:
+                
+                
