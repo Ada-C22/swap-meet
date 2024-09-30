0844a6bf96fb7e8c6cd0ee1c83c17fce0ffd2fd2
Tatiana Snook <tatsnook@gmail.com>
2024-09-30 13:38:10 -0500

    first wave

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,16 @@
 class Vendor:
     def __init__(self, inventory=None):
-        pass
+        self.inventory = []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+        
+    
