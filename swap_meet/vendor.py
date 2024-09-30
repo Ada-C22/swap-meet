05c7c0c79f1d047742255bcfbc24ac91ff309dce
Tatiana Trofimova <nerpassevera@gmail.com>
2024-09-30 12:17:05 -0700

    refactor: Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,12 @@
 class Vendor:
     def __init__(self, inventory=None):
 
-        if inventory is None:
-            self.inventory = []
-        else:
-            self.inventory = inventory
+        self.inventory = [] if inventory is None else inventory
 
     def add(self, new_item_added):
         self.inventory.append(new_item_added)
         return new_item_added
-    
+
     def remove(self, item_to_remove):
         if item_to_remove not in self.inventory:
             return False
