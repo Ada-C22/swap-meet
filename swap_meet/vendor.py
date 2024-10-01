5af2449382bf1daf7eae35a097b400b66bb90525
Priyanka <pri.ppk@gmail.com>
2024-10-01 15:46:24 -0700

    Add Wave 04 implementation

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -40,11 +40,4 @@ class Vendor:
             return True
         return False
 
-    def swap_first_item(self, other_vendor):
-        if self.inventory and other_vendor.inventory:
-            instances_first_item = self.inventory.pop(0)
-            self.inventory.append(other_vendor.inventory[0])
-            other_vendor.inventory.pop(0)
-            other_vendor.inventory.append(instances_first_item)
-            return True
-        return False
+    
