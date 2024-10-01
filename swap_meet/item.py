6b19426b37be2644d025ab5c8e2e43afee24d1f9
Priyanka <pri.ppk@gmail.com>
2024-09-30 22:12:02 -0700

    Add wave 3 and test_wave_o3 and integration_test for 1,2,3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,3 +5,8 @@ class Item:
 
     def get_category(self):
         return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,14 @@ class Vendor:
         for item in self.inventory:
             if item.id == item_id:
                 return item
+            
+    def swap_items(self, other_vendor, my_item, their_item): 
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        else:
+            self.remove(my_item)
+            other_vendor.add(my_item)
+
+            other_vendor.remove(their_item)
+            self.add(their_item)
+            return True
