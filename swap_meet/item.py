5c5937687bd4847983506574a994eb63a78ad6e1
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-01 21:29:09 +1000

    unit test wave 1 complete and all tests passed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,17 @@
+import uuid
 class Item:
-    pass
+    def __init__(self, id=uuid.uuid4().int):
+        self.id = id
+#      When we initialize an instance of `Item`, 
+#      we can optionally pass in an integer with the keyword argument `id` to manually set the `Item`'s `id`
+#      Each `Item` will have a function named `get_category`, 
+#      which will return a string holding the name of the class
+
+
+    def get_category(self):
+        return str(type(self))
+        #returns string holding name of class 
+    
+    def str(self):
+        return f"An object of type {self.get_category} with id {str(self.id)}."
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,41 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+        
+
+    def add(self,item):
+        inventory = self.inventory
+        inventory.append(item)
+        return item
+    
+    def remove(self,item):
+        if item in self.inventory:
+            inventory = self.inventory 
+            inventory.remove(item)
+            return item
+        else: 
+            return False
+    
+    def get_by_id(self,item_id):
+        for item in self.inventory: 
+            item = Item(item)
+            if item.id == item_id:
+                return item
+        return None
+
+    def swap_items(self,other_vendor,my_item,their_item):
+        other_vendor = Vendor(other_vendor)
+        my_removed_item = self.remove(my_item)
+        their_removed_item = other_vendor.remove(their_item)
+        if my_removed_item is not False and their_removed_item is not False: 
+            self.add(their_item)
+            other_vendor.add(my_item)
+            return True
+        else: 
+            if my_removed_item is not False:
+                self.add(my_item)
+            if their_removed_item is not False: 
+                other_vendor.add(their_item)
+            return False
