e3141a7d437e77c66de131101e9429ac07aca065
Sarah Koch <sarahkoch810@gmail.com>
2024-10-01 13:25:35 -0700

    Adds functionality to program through Wave 3. Passes all Wave 3 tests. Passes first integration test (Waves 1-3).
    Co-authored-by: nelasunitha <nelasunitha@users.noreply.github.com>

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,11 @@
 from uuid import uuid4
+
 class Item:
-    def __init__(self,id=None):
+    def __init__(self, id=None):
         self.id = uuid4().int if id is None else id
 
     def get_category(self):
         return type(self).__name__
+    
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}."
