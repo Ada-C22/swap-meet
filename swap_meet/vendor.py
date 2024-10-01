4c347bb90a131003543dcc631f36d64cf5e2d7f5
Anees Quateja <aneesq87@gmail.com>
2024-10-01 15:22:22 -0700

    my changes for wave_1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+
+      
+
+    def add(self, added_item):
+        self.inventory.append(added_item)
+        return added_item
+
+    
+    def remove(self, remove_item):
+        if remove_item in self.inventory:
+            self.inventory.remove(remove_item)
+            return remove_item
+        return False
