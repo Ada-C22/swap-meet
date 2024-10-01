8af0b4d7d7f7cd3d1f5148b1a545e9584ed29f14
Sarah Koch <sarahkoch810@gmail.com>
2024-09-30 20:24:32 -0700

    Adds functionality passing Wave 1 tests. Completes Wave 1 tests.
    Co-authored-by: nelasunitha <nelasunitha@users.noreply.github.com>

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
+        
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item_to_remove):
+        for item in self.inventory:
+            if item == item_to_remove:
+                self.inventory.remove(item_to_remove)
+                return item_to_remove
+        return False
