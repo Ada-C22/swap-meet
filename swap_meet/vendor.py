e5098c15c81b9dace092a73f44beecff7cb9527a
AstryBrana <astry.brana@gmail.com>
2024-09-30 23:07:14 -0400

    Wave 4 completed with tests passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -32,9 +32,16 @@ class Vendor:
             return False
     
 
-    def swap_first_item(self,):
-        pass
-
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        else:
+            other_vendor.inventory.append(self.inventory[0])
+            self.inventory.remove(self.inventory[0])
+            self.inventory.append(other_vendor.inventory[0])
+            other_vendor.inventory.remove(other_vendor.inventory[0])
+            return True
+        
     def get_by_category(self,):
         pass
 
