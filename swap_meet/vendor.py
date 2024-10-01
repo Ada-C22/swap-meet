9958170ddddc96a8a63e0b3e672b84188ae021f8
Carla Boyd <cardin_23_2@hotmail.com>
2024-09-30 22:10:24 -0700

    "Passes all tests of wave 4"

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,13 +20,24 @@ class Vendor:
                 return item
             
     def swap_items(self, other_vendor, my_item, their_item):
-        self.other_vendor = other_vendor
-        self.my_item = my_item
-        self.their_item = their_item
-
-        if their_item in self.other_vendor and my_item in self.inteventory:
-            self.other_vender.remove(their_item)
+        
+        if their_item in other_vendor.inventory and my_item in self.inventory:
+            other_vendor.inventory.remove(their_item)
             self.inventory.append(their_item)
             self.inventory.remove(my_item)
-            self.other_vender.append(my_item)
-        
+            other_vendor.inventory.append(my_item)
+            return True
+        return None
+    
+    def swap_first_item(self, other_vendor):
+        if len(self.inventory) == 0 or len(other_vendor.inventory) ==0:
+            return False
+        first_item = self.inventory[0]
+        first_item_other = other_vendor.inventory[0]
+        if  first_item  and first_item_other:
+            self.inventory.remove(first_item)
+            other_vendor.inventory.append(first_item)
+            other_vendor.inventory.remove(first_item_other)
+            self.inventory.append(first_item_other)
+            return True
+        return None
