d828ec4e78f879323626282a02736d506af3260d
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-01 13:54:50 -0700

    refactor: Wave 4 - Adds swap_helper method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -19,12 +19,15 @@ class Vendor:
             if item.id == item_id:
                 return item
 
+    def swap_helper(self, other_vendor, my_item, their_item):
+        self.inventory.remove(my_item)
+        other_vendor.inventory.append(my_item)
+        other_vendor.inventory.remove(their_item)
+        self.inventory.append(their_item)
+
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item in self.inventory and their_item in other_vendor.inventory:
-            self.inventory.remove(my_item)
-            other_vendor.inventory.append(my_item)
-            other_vendor.inventory.remove(their_item)
-            self.inventory.append(their_item)
+            self.swap_helper(other_vendor, my_item, their_item)
             return True
         return False
 
@@ -32,9 +35,6 @@ class Vendor:
         if len(self.inventory) and len(other_vendor.inventory):
             my_first_item = self.inventory[0]
             their_first_item = other_vendor.inventory[0]
-            self.inventory.remove(my_first_item)
-            other_vendor.inventory.append(my_first_item)
-            other_vendor.inventory.remove(their_first_item)
-            self.inventory.append(their_first_item)
+            self.swap_helper(other_vendor, my_first_item, their_first_item)
             return True
         return False
