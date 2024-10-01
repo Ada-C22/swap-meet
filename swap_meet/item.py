5fca3b1fa4ee0dd86b43c90e7c7e73af05e6fefe
Carla Boyd <cardin_23_2@hotmail.com>
2024-09-30 20:25:29 -0700

    "Corrects Item file"

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -7,5 +7,9 @@ class Item:
             self.id = uuid.uuid4().int
         else:
             self.id = id
+
     def get_category(self):
         return type(self).__name__ 
+    
+    def __str__(self):
+        return f'An object of type Item with id {self.id}.'
