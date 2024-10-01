ae6b7b060bbc268052b5a91cd73efbc6bfe73307
lioliadoc <liolia.doc@gmail.com>
2024-10-01 14:34:15 -0700

    Wave 3 commit

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from swap_meet.item import Item
+
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
@@ -18,4 +20,20 @@ class Vendor:
             if item.id == item_id:
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):  
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.remove(my_item)
+        other_vendor.add(my_item)
+        other_vendor.remove(their_item)
+        self.add(their_item)
+        return True
         
+        
+          
+
+
+
+
+
