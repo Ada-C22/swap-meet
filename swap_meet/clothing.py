7c73a67ab127eae742d7477b5fc829c29c1614ea
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-01 17:54:55 -0500

    Complete waves 1-5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, condition=0, fabric="Unknown", id=None):
+        super().__init__(id, condition)
+        self.fabric = fabric
+    
+    def get_category(self):
+        return "Clothing"
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,26 +1,44 @@
 class Vendor:
-    def __init__(self, inventory):
-        pass
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
         
-    def add(self):
-        pass
-    def remove(self):
-        pass
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
 
-    # Wave 2
-
-    def get_by_id(self):
-        pass
+    def get_by_id(self, item_id):
+        for item in self.inventory:
+            if item.id is item_id:
+                return item
+        return None
 
     #Wave 3
 
     def swap_items(self, other_vendor, my_item, their_item):
-        pass
+        if their_item not in other_vendor.inventory or my_item not in self.inventory:
+            return False
+        other_vendor.add(my_item)
+        self.add(their_item)
+        other_vendor.remove(their_item)
+        self.remove(my_item)
+        return True
 
     #Wave 4
 
     def swap_first_item(self, other_vender):
-        pass
+        if not self.inventory or not other_vender.inventory:
+            return False
+        my_first = self.inventory[0]
+        their_first = other_vender.inventory[0]
+        self.inventory[0]= their_first
+        other_vender.inventory[0] = my_first
+        return True
 
     #Wave 5
     
