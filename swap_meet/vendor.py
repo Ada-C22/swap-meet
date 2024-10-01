36490031bbc305a5540f79d50e4d11d78a94fbe9
nelasunitha <80846358+nelasunitha@users.noreply.github.com>
2024-09-30 21:04:42 -0700

    Adds functionality to program through Wave 2. Passes all Wave 2 tests.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,14 +1,20 @@
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
-        
+
     def add(self, item):
         self.inventory.append(item)
         return item
-    
+
     def remove(self, item_to_remove):
         for item in self.inventory:
             if item == item_to_remove:
                 self.inventory.remove(item_to_remove)
                 return item_to_remove
         return False
+
+    def get_by_id(self, id_num):
+        for item in self.inventory:
+            if item.id == id_num:
+                return item
+        return None
