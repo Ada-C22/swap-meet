6d8a5cdfe1ca8932f96bffbda3748585e2e13d2d
AstryBrana <astry.brana@gmail.com>
2024-09-30 22:04:48 -0400

    Wave 3 vendor class with relevant tests passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,8 +21,16 @@ class Vendor:
             
         return None
 
-    def swap_items(self,):
-        pass
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.inventory.remove(my_item)
+            self.inventory.append(their_item)
+            other_vendor.inventory.append(my_item)
+            other_vendor.inventory.remove(their_item)
+            return True
+        else:
+            return False
+    
 
     def swap_first_item(self,):
         pass
