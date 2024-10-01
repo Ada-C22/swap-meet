19c350d439892be546af4e1684144c630f44be95
Tatiana Snook <tatsnook@gmail.com>
2024-09-30 19:00:12 -0500

    wave 2 is completed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = id if id is not None else uuid.uuid4().int
+
+    def get_category(self):
+        return self.__class__.__name__
