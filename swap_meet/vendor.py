6533bc30c9a029f047be2c9fae42e4f02a97e501
luqi <lq.xie.work@gmail.com>
2024-10-01 15:21:01 -0700

    complete wave1 and wave2 and pass two tests.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
+from swap_meet.item import Item
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if id == item.id:
+                return item
+        return None
+
+
