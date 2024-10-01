43cfa093582f81b4879d8e7cd06c0340cbc094e6
mayboolean <>
2024-10-01 16:16:57 -0400

    creates wave 4 and passes tests

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,16 @@
-class Clothing:
-    pass
+from item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric = "Unknown"):
+        super().__init__(id=None) # parent class Item id
+        self.fabric = fabric
+    
+    # inherits Item method get_category
+
+    # overrides Item method str
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}. It is made from {self.fabric} fabric."
+    
+x = Clothing()
+print(str(x))
+    
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -11,30 +11,7 @@ class Item:
     def get_category(self):
         return self.__class__.__name__
     
-<<<<<<< HEAD
-    def __eq__(self, other):
-        if isinstance(other, Item):
-            return self.id == other.id
-        return False  
-    
-    def __str__(self):
-        return f"An object of type {self.get_category()} with id {self.id}."
-    
-i = Item()
-x = i.get_category()
-print(type(x))
-=======
     # wave 3
     def __str__(self):
-        '''
-        `item_a = Item(id=12345)`, 
-        the output of `str(item_a):
-        should be `"An object of type Item with id 12345."`'''
-       
         return f"An object of type {self.get_category()} with id {self.id}."
     
-    
-item_a = Item(id=12345)
-
-print(item_a)
->>>>>>> f9fdaa5caadc290f184820c263be25d4472e7fa1
