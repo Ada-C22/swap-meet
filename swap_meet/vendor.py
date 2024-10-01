73b579a1e0358c2c1470fbeb193db1788ee778a8
Tram <tramnhoang@gmail.com>
2024-10-01 13:23:38 -0700

    Changes to wave 2 - Tram

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,6 @@
 class Vendor:
-    def __init__(self, inventory= None):
-        inventory = [] if inventory is None else inventory
-        self.inventory = inventory
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
 
     def add(self,item):
         self.inventory.append(item)
@@ -13,3 +12,10 @@ class Vendor:
         
         self.inventory.remove(item)
         return item
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
+
