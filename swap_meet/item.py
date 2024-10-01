387d557cfd56bb9c890153ac75ad752a66869ba7
izzycommits <izzy24877@gmail.com>
2024-10-01 17:39:35 -0400

    pass test wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,8 +2,10 @@ import uuid
 
 class Item:
     def __init__(self, id=None):
-        user_uuid = uuid.uuid4()
-        self.id = user_uuid.int if id is None else id
+        self.id = uuid.uuid4().int if id is None else id
     
     def get_category(self):
         return str(self.__class__.__name__)
+    
+    def __str__(self): 
+        return "An object of type Item with id {}.".format(self.id)
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,17 @@ class Vendor:
             if item_obj.id == item_id:
                 return item_obj
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory: 
+            return False
+        if their_item not in other_vendor.inventory:
+            return False            
+        other_vendor.inventory.append(my_item)
+        self.inventory.append(their_item)
+        other_vendor.inventory.remove(their_item)
+        self.inventory.remove(my_item)
+
+        return True
+    
+    
