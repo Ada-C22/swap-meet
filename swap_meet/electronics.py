7a3f162a124e773119471f623d1a7276ef1d7e4f
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-01 15:03:05 -0700

    Wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
-class Electronics:
-    pass
+from .item import Item
+class Electronics(Item):
+    def __init__(self, id=None, type="Unknown", condition=0):
+        super().__init__(id, condition)
+        self.type = type
+
+    def __str__(self) -> str:
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,23 @@
 from uuid import uuid1
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         self.id = int(uuid1()) if id is None else id
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
 
     def __str__(self) -> str:
         return f"An object of type Item with id {self.id}."
+
+    def condition_description(self):
+        if self.condition in range(0, 6):
+            condition_descriptions = {
+                1: "Acceptable",
+                2: "Good",
+                3: "Very Good",
+                4: "Like New",
+                5: "Brand New",
+                0: "Unknown"
+            }
+            return condition_descriptions[self.condition]
