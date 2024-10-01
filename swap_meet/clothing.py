2d9f5cf578a7dc885aaf14a3b389fa1e8ba3a45e
Tami <tamigaertner@outlook.com>
2024-10-01 18:42:04 -0400

    work wave 5 cloth and decore. Need to debug

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
+from .item import Item
+
 class Clothing:
-    pass
+    def __init__(self,id=Item().id, fabric= "Unknown"):
+        
+        self.id = id
+        self.fabric = fabric
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."  
+    
+    def get_category(self):
+        return "Clothing"
+        
