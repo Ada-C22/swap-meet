89a4352c134c0f552f715c8bbc0243806de63b62
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-01 17:09:00 -0400

    updated item.py and vendor.py to fix merge conflict

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,16 +1,12 @@
 import uuid
 
 class Item:
-    def __init__(self, id = None):
+    def __init__(self, id=None):
         self.id = uuid.uuid4().int if id is None else id
         self.name = "Item"
 
+    def __str__(self):
+        return f"An object of type {self.name} with id {self.id}."
+    
     def get_category(self):
         return self.name
-
-    # def get_by_id(self, id):
-    #     inventory = Vendor.inventory
-
-    #     if id not in inventory:
-    #         return None
-    #     return Vendor.id
