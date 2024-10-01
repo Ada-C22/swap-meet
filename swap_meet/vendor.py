a22ddbba0a7091c3c288aeeedf15a577e08be211
Alejandra Guevara <alejandra.gue91@gmail.com>
2024-10-01 14:31:53 -0600

    Changed code

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,11 +13,20 @@ class Vendor:
             return item
         except ValueError:
             return False
-
-    def get_by_id(self, id):
-        if id in self.inventory:
-            return id
-        else:
-            return None
-
+    
+    
+    def get_by_id(self, item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+        return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        self.inventory.remove(my_item)
+        other_vendor.add(my_item)
+        other_vendor.remove(their_item)
+        self.inventory.add(their_item)
+        return True 
+    
+    
 
