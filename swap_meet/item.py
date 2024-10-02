187a1296c599117d347cfcab8e2fde06d35ed9cb
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-02 17:55:21 -0400

    wave 5 (implemented child classes Clothing, Decor, Electronics) with working but rough draft of condition_description()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,12 +1,26 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
-        self.id = uuid.uuid4().int if id is None else id
-        self.name = "Item"
-
+    def __init__(self, id=None, condition=0):
+        self.id = uuid.uuid4().int if not id else id
+        self.condition = condition
+    
     def __str__(self):
-        return f"An object of type {self.name} with id {self.id}."
+        return f"An object of type {self.get_category()} with id {self.id}."
     
     def get_category(self):
-        return self.name
+        return self.__class__.__name__
+    
+    # please update
+    def condition_description(self):
+        if self.condition == 5:
+            return "Mint"
+        if self.condition >= 4:
+            return "Like new"
+        if self.condition >= 3:
+            return "Very good"
+        if self.condition >= 2:
+            return "Good"
+        if self.condition >= 1:
+            return "Acceptable"
+        return "Heavily used"
