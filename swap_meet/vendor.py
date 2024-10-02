6103c5dffff14253c0b2cec03462ca8cbd53cede
Anees Quateja <aneesq87@gmail.com>
2024-10-02 13:25:09 -0700

    implement wave_4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,17 +21,42 @@ class Vendor:
                 return item
         return None
     
+
+                    #wave_3
+    
     def swap_items(self, other_vendor, my_item, their_item):
 
         if not my_item in self.inventory or not their_item in other_vendor.inventory:
             return False
         
 
-        self.remove(my_item) 
-        other_vendor.add(my_item)
-        self.add(their_item) 
-        other_vendor.remove(their_item)
+        self.remove(my_item) #removes `my_item` from this `Vendor`'s inventory
+        other_vendor.add(my_item) #adds it to the friend's inventory
+        self.add(their_item) #adds it to this `Vendor`'s inventory
+        other_vendor.remove(their_item) #removes `their_item` from the other `Vendor`'s inventory
 
         return True
+    
 
+                    # Wave_4
+
+    def swap_first_item(self, other_vendor):
+
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        
+        # Remove the first item from both vendors' inventories
+        self_first_item  = self.inventory.pop(0)
+        others_first_item = other_vendor.inventory.pop(0)
+
+        # Add the first item from the other vendor to each vendor's inventory
+        self.inventory.insert(0, others_first_item)
+        other_vendor.inventory.insert(0,self_first_item)
+
+        # Return True to indicate the swap was successful
+        return True
+    
+
+                    # Wave_5
+            
 
