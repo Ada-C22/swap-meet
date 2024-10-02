5359bcc87d6d98aeeec05cffa79a2c0e1377cafc
Leidy <leidymarbu@gmail.com>
2024-10-02 16:29:29 -0400

    fixed vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,4 @@
-from .item import Item
+from swap_meet.item import Item
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
@@ -64,18 +64,3 @@ class Vendor:
                 
         
         return item
-
-    def swap_items(self, other_vendor, my_item, their_item):
-        if my_item in self.inventory and their_item in other_vendor.inventory:
-            self.inventory.remove(my_item)
-            other_vendor.inventory.append(my_item)
-            other_vendor.inventory.remove(their_item)
-            self.inventory.append(their_item)
-            return True
-        return False
-    
-    def swap_first_item(self, other_vendor):
-        if self.inventory and other_vendor.inventory:
-            self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
-            return True
-        return False
