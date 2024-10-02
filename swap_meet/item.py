75cbc3de540e6c9baf1d537001e8b51da302f809
Priyanka <pri.ppk@gmail.com>
2024-10-01 21:21:35 -0700

    Add Wave 05 and test_wave_05

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,26 @@
 from uuid import uuid1
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0.0):
         self.id = int(uuid1()) if id is None else id
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"An object of type Item with id {self.id}."
+
+    def condition_description(self):
+        # Round to nearest whole number
+        round_condition = round(self.condition)
+        descriptions = {
+            0: "It's basically a wreck. You might want to steer clear of this one.",
+            1: "Heavily used. You probably want a glove for this one...",
+            2: "Well-loved, but still hanging in there.",
+            3: "In good condition! Ready for a new home.",
+            4: "Like new! You can hardly tell it’s been used.",
+            5: "Mint condition! An absolute treasure."
+        }
+
+
+        return descriptions.get(round_condition, "Condition unrecognized")
