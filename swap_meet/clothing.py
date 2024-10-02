decf58030fc50f9f7951f193fb01212b287279c4
lioliadoc <liolia.doc@gmail.com>
2024-10-01 21:05:52 -0700

    Wave 5 commit

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,17 @@
-class Clothing:
-    pass
+import uuid
+
+from swap_meet.item import Item 
+
+class Clothing(Item):
+
+    def __init__(self, id=None, condition=0, fabric="Unknown"):
+        super().__init__(id, condition)
+        self.fabric = fabric
+
+    
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+    
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,14 @@
-class Electronics:
-    pass
+import uuid
+from swap_meet.item import Item
+
+class Electronics(Item):
+    
+    def __init__(self, id=None, condition=0, type="Unknown"):
+        super().__init__(id, condition)
+        self.type = type
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
