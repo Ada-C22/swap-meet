9670f199a7c98ed7e55da5f2643b695988a63a21
Tatiana Snook <tatsnook@gmail.com>
2024-10-01 19:20:14 -0500

    wave4 is completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,6 @@
 from swap_meet.item import Item
 
+
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
@@ -14,14 +15,14 @@ class Vendor:
             return item
         else:
             return False
-        
+
     def get_by_id(self, item_id):
         for item in self.inventory:
             if item.id == item_id:
                 return item
         return None
-    
-    def swap_items(self, other_vendor, my_item, their_item):  
+
+    def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
         self.remove(my_item)
@@ -29,11 +30,15 @@ class Vendor:
         other_vendor.remove(their_item)
         self.add(their_item)
         return True
-        
-        
-          
 
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
 
+        first_item = self.remove(self.inventory[0])
+        other_vendor.add(first_item)
 
+        other_first_item = other_vendor.remove(other_vendor.inventory[0])
+        self.add(other_first_item)
 
-
+        return True
