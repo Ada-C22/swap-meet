5cac43d014cb486aec487eef2656f04d4a647b60
Madina-j <dzhetegenova@gmail.com>
2024-10-02 15:43:55 -0700

    implemented Wave_5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
-class Clothing:
-    pass
+from .item import Item
+class Clothing(Item):
+    def __init__(self, id, fabric= "Unknown"):
+        super().__init__(id, condition=0)
+        self.fabric = fabric
+        self.id = id
+
+
+    def get_category():
+        return "Clothing"
+    
+    def __str__(self):
+        return (f"An object of type Clothing with id {self.id}.It is made from {self.fabric} fabric.")
+    
