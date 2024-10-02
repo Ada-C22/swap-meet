67f01e7eb6e6dbed2849ca5683ded913588ef7b2
Madina-j <dzhetegenova@gmail.com>
2024-10-02 12:28:01 -0700

    implemented str function, coment decorators

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,12 +1,6 @@
-import uuid 
 class Item:
-    def __init__(self, id=None): 
-        #each item should have a unique id using uuid
-        self.id = uuid.uuid4().int
-    
-        self.category = str
-    def get_category(self):
-        # return #string of class
-        self.category = "Clothing"
-        self.category = "Decor"
-        self.category = "Electronics"
+    pass
+
+def __str__(self):
+        id_str = str(self.id)
+        return (f"An object of type Item with id {id_str}.")
