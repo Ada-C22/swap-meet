be875e27f24f2614eac3c3afb4efe1df0a82e076
Tram <tramnhoang@gmail.com>
2024-10-02 05:22:06 -0700

    complete wave 4-5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,12 @@
-class Decor:
-    pass
+from .item import Item
+
+class Decor(Item):
+    def __init__(self, id=None, width=None, length=None, condition=None):
+        super().__init__(id, condition)
+        self.width = 0 if width is None else width
+        self.length = 0 if length is None else length
+    def get_category(self):
+        return "Decor"
+    def __str__(self):
+        return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
+
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,26 +1,25 @@
-<<<<<<< HEAD
 import uuid 
+
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=None):
         self.id = id if id is not None else uuid.uuid4().int
+        self.condition = condition if condition is not None else 0
     def get_category(self):
         return type(self).__name__
-
-=======
-import uuid
-
-class Item:
-
-    def __init__(self, id = None):
-        # Generate a random UUID
-        random_uuid = uuid.uuid4()
-
-        # Convert UUID to a random integer
-        random_int = random_uuid.int
-
-        id = random_int if id is None else id
-        self.id = id
-
-    def get_category(self):
-        return self.__class__.__name__
->>>>>>> 41c4f9a66446fdf5da871c2087fda1ef9a57043f
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+    def condition_description(self):
+        if self.condition == 0:
+            return "You should seriously consider not taking this one"
+        elif self.condition < 1:
+            return "It's barely holding together."
+        elif self.condition < 2:
+            return "This item has seen better days."
+        elif self.condition < 3:
+            return "It is a bit worn, but still has life left."
+        elif self.condition < 4:
+            return "In good condition"
+        elif self.condition < 5:
+            return "Almost like new!"
+        else:
+            return "Mint condition"
