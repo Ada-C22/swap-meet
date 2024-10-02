b6efc49f464ff4a1500d89671a8184627c716ff5
Natalie <natali3sen@gmail.com>
2024-10-02 15:15:03 -0500

    finished wave 4&5 and tweaked a bit of wave 1 to pass the integration test

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,14 +2,26 @@ import uuid
 
 class Item:
     
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id == None:
             self.id = uuid.uuid4().int
         else:
             self.id = id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
+    
+    def condition_description(self):
+        descriptions = {
+            0: "Ew no",
+            1: "Look at other things to save time",
+            2: "Very much used, but it's cheap",
+            3: "Condition is acceptable for the worth",
+            4: "Close to almost unused",
+            5: "Almost unused"
+        }
+        return descriptions.get(self.condition, "Unknowned condition.")
     
     def __str__(self):
         return f"An object of type Item with id {self.id}."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,11 @@
+
 class Vendor:
 
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
-
+    def __init__(self, inventory=None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
     def add(self, item):
         self.inventory.append(item)
         return item
@@ -24,12 +27,20 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
 
         if my_item in self.inventory and their_item in other_vendor.inventory:
-            self.inventory.remove(my_item)
-            other_vendor.inventory.append(my_item)
-
-            other_vendor.inventory.remove(their_item)
-            self.inventory.append(their_item)
-            return True
-        else:
+            self.remove(my_item)
+            other_vendor.remove(their_item)
+            self.add(their_item)
+            other_vendor.add(my_item)
+    
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
             return False
+
+        self_first_item = self.inventory.pop(0)
+        other_first_item = other_vendor.inventory.pop(0)
+
+        self.inventory.append(other_first_item)
+        other_vendor.inventory.append(self_first_item)
+
+        return True
             
