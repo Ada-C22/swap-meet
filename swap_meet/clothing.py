edd8e74051df2de444e03ce48a1b86a3f80b3612
Leidy <leidymarbu@gmail.com>
2024-10-02 19:57:31 -0400

    fixed Clothing parameters

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, id=None, width=None, length=None, condition=None, fabric="Unknown", age=None):
+    def __init__(self, id=None, condition=None, fabric="Unknown", age=None):
         super().__init__(id, condition, age)
         self.fabric = fabric
 
