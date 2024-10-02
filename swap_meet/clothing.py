92a2c80c9d0c9a8badc7f5c8f4252f442639d674
Jenny Massari <jennymartins13@gmail.com>
2024-10-02 14:38:58 -0700

    Added Clothing Class for wave 5 .

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,3 +1,17 @@
-# Hi Brianna :)
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+
+    def __init__(self, id=None, fabric="Unknown"):
+        
+        super().__init__(id)
+        self.fabric = fabric
+        # self.fabric = fabric if not fabric is "Unknown" else fabric
+
+    def get_category(self):
+        return "Clothing" 
+
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,8 +2,6 @@ class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
 
-    # def __init__(self, inventory=[]):
-    #     self.inventory = inventory
 
     def add(self, item):
         self.inventory.append(item)
@@ -22,6 +20,7 @@ class Vendor:
                 return item
         return None
     
+
     def swap_items(self, other_vendor, my_item, their_item):
         """
         If this Vendor's inventory contain my_item and 
@@ -71,3 +70,72 @@ class Vendor:
 # fatimah = Vendor(inventory=["item_a", "item_b", "item_c"])
 
 # print(mine.swap_first_item(fatimah))
+
+
+
+
+
+
+    
+#     def swap_items(self, other_vendor, my_item, their_item):
+#         """
+
+        
+#         If this Vendor's inventory contain my_item and 
+#         the friend's inventory contain their_item. 
+
+        
+                    
+                                
+                                
+                                            
+                                                        
+                                                                    
+                                                                                
+                                                                                            
+                                                                                                        
+                                                                                                                    
+                                                                                                                                
+#                                                                                                                                                         self.inventory.remove(my_item) 
+#             other_vendor.inventory.append(my_item)
+#             # if their_item in other_vendor.inventory:
+#             other_vendor.remove(their_item) 
+#             self.inventory.append(their_item)
+#             return True
+#         else: 
+#             return False
+            
+#     def swap_first_item(self, other_vendor):
+
+#         """
+#         Function swaps my first item with their first item.
+        
+#         Input: other vendor
+
+#         Output: Returns either True or False. 
+#         - Returns False if my inventory or their inventory is empty. 
+#         - Otherwise, returns True.        
+#         """
+
+#         if not self.inventory or not other_vendor.inventory:
+#             return False
+        
+
+
+#         first_item_in_my_inventory = self.inventory[0]
+#         first_item_in_vendor_inventory = other_vendor.inventory[0]
+        
+#         # swap_item = Vendor.swap_items(self, other_vendor, first_item_in_my_inventory, first_item_in_vendor_inventory)
+#         # print(self.inventory)
+#         # print(other_vendor.inventory)
+
+#         self.inventory[0] = first_item_in_vendor_inventory
+#         other_vendor.inventory[0] = first_item_in_my_inventory
+        
+#         return True
+
+
+# # mine = Vendor(inventory=["item_x", "item_y", "item_z"])
+# # fatimah = Vendor(inventory=["item_a", "item_b", "item_c"])
+
+# # print(mine.swap_first_item(fatimah))
