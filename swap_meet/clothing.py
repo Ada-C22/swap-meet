60c37497e68cc4cb7a3aa8ef5f1404512945b852
Jenny Massari <jennymartins13@gmail.com>
2024-10-02 16:44:55 -0700

    Added Electronics Class, and inheriting condition from Item Class to the 3 classes of wave 5.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,9 +2,9 @@ from swap_meet.item import Item
 
 class Clothing(Item):
 
-    def __init__(self, id=None, fabric="Unknown"):
+    def __init__(self, id=None, fabric="Unknown", condition=0):
         
-        super().__init__(id)
+        super().__init__(id, condition)
         self.fabric = fabric
         # self.fabric = fabric if not fabric is "Unknown" else fabric
 
