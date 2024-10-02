11254c8e41daa7b3101cc5ee16e452ccc9d9817e
anhtran <anhtr077@gmail.com>
2024-10-01 23:32:03 -0700

    add Parent class to child class and adding assertion to test wave 6

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,11 +1,10 @@
 from swap_meet.item import Item
 
-class Decor:
+class Decor(Item):
     def __init__(self, id=None, condition=0, width=0, length=0):
         super().__init__(id, condition) # parent class Item id
         self.width = width
         self.length = length
     
     def __str__(self):
-        return f"An object of type {self.get_category()} with id {self.id}. \
-            It takes up a {self.width} by {self.length} sized space."
+        return f"An object of type {self.get_category()} with id {self.id}. It takes up a {self.width} by {self.length} sized space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,10 +1,9 @@
 from swap_meet.item import Item
 
-class Electronics:
+class Electronics(Item):
     def __init__(self, id=None, condition=0, type="Unknown"):
         super().__init__(id, condition) # parent class Item id
         self.type = type
     
     def __str__(self):
-        return f"An object of type {self.get_category()} with id {self.id}. \
-            This is a {self.type} device."
+        return f"An object of type {self.get_category()} with id {self.id}. This is a {self.type} device."
