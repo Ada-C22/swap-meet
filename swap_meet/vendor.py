385ba2eed35c1f364f14af126ab3b81e0594c0ea
Lula <mlusan95@gmail.com>
2024-10-02 16:38:30 -0400

    wave 3 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,7 +21,19 @@ class Vendor:
             if item.id == id:
                 return item
 
-        
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            self.inventory += [their_item]
+            other_vendor.inventory.remove(their_item)
+            other_vendor.inventory += [my_item]
+        return True
+
+
+
+
     
        
 
