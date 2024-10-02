444ea8c486aa1a063a9a497a7e95dfecd236c5de
Madina-j <dzhetegenova@gmail.com>
2024-10-02 13:31:40 -0700

    implement test wave3, implement swap_items function

--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -131,7 +131,8 @@ def test_swap_items_from_their_empty_returns_false():
 
     result = fatimah.swap_items(jolie, item_b, nobodys_item)
 
-    raise Exception("Complete this test according to comments below.")
-    # *********************************************************************
-    # ****** Complete Assert Portion of this test **********
-    # *********************************************************************
+    assert result == False
+    # raise Exception("Complete this test according to comments below.")
+    # # *********************************************************************
+    # # ****** Complete Assert Portion of this test **********
+    # # *********************************************************************
