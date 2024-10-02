7ea05f08fbf46c028ba883c637bf93093eb0e01f
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-02 01:41:46 -0400

    consolidated swap_items and updated stylization

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,8 +21,8 @@ class Vendor:
         return None
     
     def swap_items(self,other_vendor,my_item,their_item):
-        if my_item not in self.inventory or \
-            their_item not in other_vendor.inventory:
+        if (my_item not in self.inventory or
+            their_item not in other_vendor.inventory):
             return False
         
         self.remove(my_item)
@@ -32,26 +32,9 @@ class Vendor:
         self.add(their_item)
 
         return True
-    
-    # def swap_items(self, other_vendor, my_item, their_item):
-    #     if my_item not in self.inventory or their_item not in other_vendor.inventory:
-    #         return False
-        
-    #     for i in range(len(self.inventory)):
-    #         if self.inventory[i] == my_item:
-    #             self.inventory[i] = their_item
-    #             break
-        
-    #     for j in range(len(other_vendor.inventory)):
-    #         if other_vendor.inventory[j] == their_item:
-    #             other_vendor.inventory[j] = my_item
-    #             break
-
-    #     return True
 
     def swap_first_item(self,other_vendor):
         my_first = self.inventory[0]
         their_first = other_vendor.inventory[0]
 
         result = self.swap_items(other_vendor,my_first,their_first)
-
