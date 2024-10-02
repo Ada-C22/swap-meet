ed62e670dc69ecea137190e3af276d704f326000
Tatiana Snook <tatsnook@gmail.com>
2024-10-02 17:31:23 -0500

    all tests are passed

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -253,7 +253,7 @@ def test_swap_best_by_category_no_match_is_false():
     # - That all the correct items are in tai and jesse's inventories
     assert not result
     assert len(tai.inventory) == 3
-    assert len(jesse.inventory) == 0
+    assert len(jesse.inventory) == 3
     assert item_a in tai.inventory
     assert item_b in tai.inventory
     assert item_c in tai.inventory
@@ -262,7 +262,7 @@ def test_swap_best_by_category_no_match_is_false():
     assert item_f in jesse.inventory
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_other_match_is_false():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -286,7 +286,7 @@ def test_swap_best_by_category_no_other_match_is_false():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    # raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
@@ -294,3 +294,12 @@ def test_swap_best_by_category_no_other_match_is_false():
     # - That result is falsy
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories
+    assert not result
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert item_a in tai.inventory
+    assert item_b in tai.inventory
+    assert item_c in tai.inventory
+    assert item_d in jesse.inventory
+    assert item_e in jesse.inventory
+    assert item_f in jesse.inventory
