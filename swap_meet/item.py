55c8e769390764e842a53b26979c428340b48fd5
Lula <mlusan95@gmail.com>
2024-10-02 17:43:44 -0400

    clothing passed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,12 +1,21 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         id = int(uuid.uuid4()) if not id else id
         self.id = id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
     
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+    
+    def condition_description(self):
+        conditional_phrases = ["yikes", "noth worth it", "it could work", "just a scratch",
+                                "almost new", "as good as it gets"]
+        return conditional_phrases[self.condition]
+    
+    
+        
