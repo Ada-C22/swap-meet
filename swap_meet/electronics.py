8f5076f5e166a2df70fe089a075fedfd0f8bf0fc
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-03 13:16:50 +1000

    electronics type added, all tests passed

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,3 +1,8 @@
 from swap_meet.item import Item
-class Electronics:
-    pass
+class Electronics(Item):
+    def __init__(self, id = None, type="Unknown"):
+        super().__init__(id)
+        self.type = type
+
+    def __str__(self):
+        return super().__str__() + f" This is a {self.type} device."
