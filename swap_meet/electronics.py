4f0ac59b6855a468e61683b6655b96ed01b372c6
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-03 16:27:08 -0700

    chore: Adds docstring to Electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,7 +1,27 @@
 from .item import Item
 
 class Electronics(Item):
-    def __init__(self, id=None, condition=0,type="Unknown", age=None):
+    """
+    A subclass of Item that represents an item of decor.
+
+    Parameters:
+    ----------
+    id : int, optional
+        Id of the item. Generated randomly by default
+    condition : int, optional
+        An integer value representing the condition of the item,
+        where 1 is the worst and 5 is the best. Must be between 1 and 5.
+        Defaults to 0, indicating that no condition is unknown.
+    type: str
+        A string value representing type of the electrinc item.
+        Defaults to "Unknown"
+    age : tuple, optional
+        A tuple of 3 integer representing date in format (yyyy, mm, dd).
+        Defaults to current day.
+
+    """
+
+    def __init__(self, id=None, condition=0, type="Unknown", age=None):
         super().__init__(id, condition,age)
         self.type = type
 
