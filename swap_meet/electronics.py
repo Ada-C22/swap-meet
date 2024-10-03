08f0961396dceae8fda86d713ae3d10e3418f62a
Christelle Nkera <scnkera@gmail.com>
2024-10-03 02:34:38 -0400

    Wave 5: added an Electronics sub class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,11 @@
-class Electronics:
-    pass
+from .item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None, condition=0, width=0, length=0):
+        super().__init__(id, condition)
+        self.type = type
+    def get_category(self):
+        return "Electronics"
+    
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
