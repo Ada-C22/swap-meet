867a9fc5c823bf7db4c43dd2f761fcfe3332acf1
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-03 16:20:23 -0700

    chore: Adds docstring to Decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,7 +1,29 @@
 from .item import Item
 
 class Decor(Item):
-    def __init__(self,id=None, condition=0,width=0, length=0, age=None):
+    """
+    A subclass of Item that represents an item of decor.
+
+    Parameters:
+    ----------
+    id : int, optional
+        Id of the item. Generated randomly by default
+    condition : int, optional
+        An integer value representing the condition of the item,
+        where 1 is the worst and 5 is the best. Must be between 1 and 5.
+        Defaults to 0, indicating that no condition is unknown.
+    width : int, optional
+        An integer value representing width of the item.
+        Defaults to 0.
+    length : int, optional
+        An integer value representing length of the item.
+        Defaults to 0.
+    age : tuple, optional
+        A tuple of 3 integer representing date in format (yyyy, mm, dd).
+        Defaults to current day.
+
+    """
+    def __init__(self,id=None, condition=0, width=0, length=0, age=None):
         super().__init__(id, condition, age)
         self.width = width
         self.length = length
