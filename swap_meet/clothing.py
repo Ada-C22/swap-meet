39391bf809b8aeeb24bf50028b0887516bb90d51
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-03 13:25:29 +1000

    condition statements added, and tests passed

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 class Clothing(Item):
-    def __init__(self, id = None, fabric="Unknown"):
-        super().__init__(id)
+    def __init__(self, id=None, fabric="Unknown", condition=0):
+        super().__init__(id, condition)
         self.fabric = fabric
     def __str__(self):
         return super().__str__() + f" It is made from {self.fabric} fabric."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,7 @@
 import uuid
 class Item:
-    def __init__(self,id=None):
+    def __init__(self, id=None, condition=0):
+        self.condition = condition
         if id == None: 
             self.id = uuid.uuid4().int
         else:
@@ -16,3 +17,16 @@ class Item:
         str_id= str(self.id)
         return f"An object of type {category} with id {str_id}."
     
+    def condition_description(self):
+        if self.condition == 0: 
+            return "This will leave a film on you"
+        elif self.condition == 1: 
+            return "Definitely wash hands after touching me"
+        elif self.condition == 2: 
+            return "Might still smell after washing"
+        elif self.condition == 3: 
+            return "How we all feel once we turn 35"
+        elif self.condition == 4:
+            return "Feeling young and confident yet still slightly jaded about the world"
+        elif self.condition == 5: 
+            return "Fresh as a daisy on a dewey spring morning"
