2da24db954d45e71d87d1a04e78bbfbffc7e0a30
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-03 16:23:11 -0700

    feat: Adds age string to Electronics.__str__

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -27,7 +27,7 @@ class Decor(Item):
         super().__init__(id, condition, age)
         self.width = width
         self.length = length
-    
+
     def __str__(self):
         return  f"An object of type Decor with id {self.id}. "\
                 f"It takes up a {self.width} by {self.length} sized space. "\
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -6,5 +6,6 @@ class Electronics(Item):
         self.type = type
 
     def __str__(self):
-        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."    
-        
+        return  f"An object of type Electronics with id {self.id}. "\
+                f"This is a {self.type} device."\
+                f"It was produced on {self.age.strftime("%x")}."
