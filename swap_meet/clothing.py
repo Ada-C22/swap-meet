e204d186c53c53ee504bc2b073e25ade8f18f2b0
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-03 16:13:25 -0700

    chore: Adds docstring for Clothing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,6 +1,25 @@
 from .item import Item
 
 class Clothing(Item):
+    """
+    A subclass of Item that represents an item of clothing.
+
+    Parameters:
+    ----------
+    id : int, optional
+        Id of the item. Generated randomly by default
+    fabric : str, optional
+        Type of fabric the item is made of. Defaults to "Unknown"
+    condition : int, optional
+        An integer value representing the condition of the item, 
+        where 1 is the worst and 5 is the best. Must be between 1 and 5. 
+        Defaults to 0, indicating that no condition is unknown.
+    age : tuple, optional
+        A tuple of 3 integer representing date in format (yyyy, mm, dd).
+        Defaults to current day.
+
+    """
+
     def __init__(self, id=None, fabric="Unknown", condition=0, age=None):
         super().__init__(id, condition, age)
         self.fabric = fabric
