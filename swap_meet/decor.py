1d5f0a46ec539d97f9fab3d422b737a498511c5e
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-03 13:11:04 +1000

    decor type added, all decor tests passed

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,3 +1,8 @@
 from swap_meet.item import Item
-class Decor:
-    pass
+class Decor(Item):
+    def __init__(self,id = None, width = 0, length = 0):
+        super().__init__(id)
+        self.width = width
+        self.length = length
+    def __str__(self):
+        return super().__str__() + f" It takes up a {self.width} by {self.length} sized space."
