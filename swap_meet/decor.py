9dc977e6564f8e1acd190bd5f62bf25b535b51b5
Priyanka <pri.ppk@gmail.com>
2024-10-03 17:42:50 -0700

    Adjust f string line 31 to single quotes for Decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -29,6 +29,6 @@ class Decor(Item):
         self.length = length
 
     def __str__(self):
-        return  f"An object of type Decor with id {self.id}. "\
-                f"It takes up a {self.width} by {self.length} sized space. "\
-                f"It was produced on {self.age.strftime("%x")}."
+        return  f"An object of type Decor with id {self.id}."\
+                f"It takes up a {self.width} by {self.length} sized space."\
+                f"It was produced on {self.age.strftime('%x')}."
