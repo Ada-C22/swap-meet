5773823f1bc78c6a0ce449004936b5a2aefebe1b
Leidy <leidymarbu@gmail.com>
2024-10-04 13:59:54 -0400

    Rfactored if

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -13,16 +13,16 @@ class Item:
         return f"An object of type {self.get_category()} with id {self.id}."
     
     def condition_description(self):
-        
+
         if self.condition > 0 and self.condition < 1:
             return "It's barely holding together."
-        if self.condition >= 1 and self.condition < 2:
+        elif self.condition >= 1 and self.condition < 2:
             return "This item has seen better days."
-        if self.condition >= 2 and self.condition < 3:
+        elif self.condition >= 2 and self.condition < 3:
             return "It is a bit worn, but still has life left."
-        if self.condition >= 3 and self.condition < 4:
+        elif self.condition >= 3 and self.condition < 4:
             return "In good condition."
-        if self.condition >= 4 and self.condition < 5:
+        elif self.condition >= 4 and self.condition < 5:
             return "Almost like new!"
         return "Mint condition."
 
