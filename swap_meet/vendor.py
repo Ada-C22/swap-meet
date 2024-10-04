9893af5e01d5bc71b991984562f568c660261197
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-03 21:40:42 -0400

    formatting

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -33,18 +33,12 @@ class Vendor:
     def swap_first_item(self, other_vendor):
         if not self.inventory or not other_vendor.inventory:
             return False
-        return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
+        return self.swap_items(other_vendor, self.inventory[0], 
+                               other_vendor.inventory[0])
 
     def get_by_category(self, category):
-        return [item for item in self.inventory if item.get_category() == category]
-    
-    def swap_first_item(self, other_vendor):
-        if not self.inventory or not other_vendor.inventory:
-            return False
-        return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
-
-    def get_by_category(self, category):
-        return [item for item in self.inventory if item.get_category() == category]
+        return [item for item in self.inventory
+                if item.get_category() == category]
 
     def get_best_by_category(self, category):
         highest_item = None
