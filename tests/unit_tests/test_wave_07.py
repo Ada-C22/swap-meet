3694e0dd0dc4ddd4f59a2477533cedf0c4b4d729
Tram <tramnhoang@gmail.com>
2024-10-03 17:19:49 -0700

    Review and add two tests for wave07

--- tests/unit_tests/test_wave_07.py
+++ tests/unit_tests/test_wave_07.py
@@ -37,3 +37,53 @@ def test_swap_newest_item():
     assert item_d in tai.inventory
     assert item_a in jesse.inventory
     
+def test_swap_newest_no_inventory_is_false():
+    tai = Vendor(
+        inventory=[]
+    )
+
+    item_a = Clothing(age=2.0)
+    item_b = Decor(age=7.0)
+    item_c = Clothing(age=4.0)
+    jesse = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    result = tai.swap_best_by_category(
+        other_vendor=jesse,
+        my_priority="Clothing",
+        their_priority="Decor"
+    )
+
+    assert not result
+    assert len(tai.inventory) == 0
+    assert len(jesse.inventory) == 3
+    assert item_a in jesse.inventory
+    assert item_b in jesse.inventory
+    assert item_c in jesse.inventory
+
+# @pytest.mark.skip
+def test_swap_newest_no_other_inventory_is_false():
+    item_a = Clothing(age=2.0)
+    item_b = Decor(age=4.0)
+    item_c = Clothing(age=4.0)
+    tai = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    jesse = Vendor(
+        inventory=[]
+    )
+
+    result = tai.swap_best_by_category(
+        other_vendor=jesse,
+        my_priority="Decor",
+        their_priority="Clothing"
+    )
+
+    assert not result
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 0
+    assert item_a in tai.inventory
+    assert item_b in tai.inventory
+    assert item_c in tai.inventory
